07d8861734f602503c182bf7e3827858594e9a7b
Matti Tahvonen <matti@vaadin.com>
2015-09-22 21:38:38 +0300

    Updated versions to Vaadin Spring 1.0, removed hack needed with pre-releases

--- src/main/java/org/vaadin/tkspring/Application.java
+++ src/main/java/org/vaadin/tkspring/Application.java
@@ -1,15 +1,11 @@
 package org.vaadin.tkspring;
 
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.spring.boot.internal.VaadinServletConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-// Make config here (vaadinServlet) override stuff in VaadinServletConfiguration
-@Import(VaadinServletConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
